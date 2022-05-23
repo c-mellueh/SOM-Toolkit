08d68efc75674fe92839c09e9da5457e312fff96
ChristophMellueh <christoph@mellueh.de>
2022-05-23 12:15:08 +0200

    create executable

--- desiteRuleCreator/main_window.py
+++ desiteRuleCreator/main_window.py
@@ -45,7 +45,7 @@ class MainWindow(QMainWindow):
 
         # debug: preload file
         #self.openFile(path="E:/Cloud/OneDrive/Arbeit/DB_Werkstudent/Projekte/Karlsruhe_Durmersheim/Modelchecking/Regeln/Datenstruktur/22_04_18.xml")
-        self.openFile("desiteRuleCreator/saves/22_04_18.xml")
+        #self.openFile("desiteRuleCreator/saves/22_04_18.xml")
         self.ui.tree.resizeColumnToContents(0)
         self.save_path = None
         print(self.save_path)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from desiteRuleCreator.main_window import main as run_main
+from  lxml import *
+from jinja2 import *
 
 def main():
     run_main()
