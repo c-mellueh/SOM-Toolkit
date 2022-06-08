7880da2603904262ff993b363f048611d5a11f1f
ChristophMellueh <christoph@mellueh.de>
2022-06-08 12:52:27 +0200

    add link icon

--- desiteRuleCreator/icons/__init__.py
+++ desiteRuleCreator/icons/__init__.py
@@ -5,6 +5,10 @@ def get_icon():
     icon_path = os.path.join(ICON_PATH, ICON_DICT["icon"])
     return QIcon(icon_path)
 
+def get_link_icon():
+    icon_path = os.path.join(ICON_PATH, ICON_DICT["link"])
+    return QIcon(icon_path)
+
 ICON_PATH = os.path.dirname(__file__)
 ICON_DICT = {"icon": "icon.ico",
              "link": "link.png"}
