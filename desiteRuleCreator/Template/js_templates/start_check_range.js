682a7bb04ad0c5f66599e3f17447d235379652f6
ChristophMellueh <christoph@mellueh.de>
2022-07-12 15:06:35 +0200

    improve js rules

--- desiteRuleCreator/Template/js_templates/start_check_range.js
+++ desiteRuleCreator/Template/js_templates/start_check_range.js
@@ -1,12 +1,26 @@
-function check_range(name, pSet, return_format, range) {
+function check_for_single(value, range) {
+    var treffer = 0;
+    for (k in range) {
+        pair = range[k]
+        if (pair.indexOf("") > -1 || pair.length < 2) {
 
+            for (t in pair) {
+                if (pair[t] == value) {
+                    treffer += 1
+                }
+            }
+        }
+    }
+    return treffer
+}
+
+function check_range(name, pSet, return_format, range) {
     //Kontrolliert, ob sich 	 pSet+name in einem Bestimmten Wertebereich bewegen
 
     //range[*][0] -> untere Grenze
     //range[*][1] -> obere Grenze
-
+    
     //Ausgabetext	
-
     var text1 = 'Eigenschaft "';
     var text2 = '" nicht vorhanden! ';
     var text3 = '" liegt außerhalb des vorgegebenen Wertebereichs in MEM! ';
@@ -16,6 +30,8 @@ function check_range(name, pSet, return_format, range) {
 
     //Attribut wird aus Objekt gelesen
     var value = desiteAPI.getPropertyValue(id, svalue, return_format);
+
+
     //Kontrolle, ob Attribut existiert		
     if (value == undefined) {
         if (!check_datatype(name, pSet, return_format)) {
@@ -29,11 +45,13 @@ function check_range(name, pSet, return_format, range) {
 
         //Wenn das Format xs:string ist, wird das Attribut in eine Float Zahl umgewandelt
         if (return_format == "xs:string") {
-            value = parseFloat(value.replace(',', '.'))
+            parsed_value = value
+            replaced = parsed_value.replace(',', '.')
+            parsed_value = parseFloat(replaced)
+            if (!isNaN(parsed_value)) {
+                value = parsed_value;
+            }
         }
-
-
-
         //Anzahl der Wertebereiche
 
         var treffer = 0;
@@ -41,29 +59,24 @@ function check_range(name, pSet, return_format, range) {
         if (desiteAPI.getPropertyUnit(svalue, return_format) == "m") {
             value = value / 1000
         }
+        treffer += check_for_single(value, range)
 
         for (i = 0; i < range.length; i++) {
-            if (range[i].length == 1) {
-                if (value == range[i][0]) {
-                    treffer += 1
-                }
-            } else {
-                var untere_grenze = range[i][0];
-                var obere_grenze = range[i][1];
+            var untere_grenze = parseFloat(range[i][0].replace(",", "."));
+            var obere_grenze = parseFloat(range[i][1].replace(",", "."));
 
-                if (value >= untere_grenze && value <= obere_grenze) {
-                    treffer += 1
-                }
+            if (value >= untere_grenze && value <= obere_grenze) {
+                treffer += 1
             }
         }
+    }
 
-        if (treffer != 1) {
-            desiteResult.addMessage(text1 + svalue + text3 + text5 + "2]");
-            return 1;
-        }
-        //Kontrolle, ob das Attribt im Wertebereich liegt
-        else {
-            return 0
-        }
+    if (treffer != 1) {
+        desiteResult.addMessage(text1 + svalue + text3 + text4 + value + ")" + text5 + "2]");
+        return 1;
+    }
+    //Kontrolle, ob das Attribt im Wertebereich liegt
+    else {
+        return 0
     }
 }
