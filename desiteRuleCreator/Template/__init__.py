2fb3702db1e1a406ba777d70263f72967a159237
ChristophMellueh <christoph@mellueh.de>
2022-10-10 10:34:28 +0200

    add IFC 4.1 Completer

--- desiteRuleCreator/Template/__init__.py
+++ desiteRuleCreator/Template/__init__.py
@@ -2,3 +2,6 @@ import os
 
 HOME_DIR = os.path.dirname(__file__)
 TEMPLATE = "template.txt"
+
+IFC_PATH = os.path.join(os.path.dirname(__file__),"ifc_classes")
+IFC_4_1 = list(open(os.path.join(IFC_PATH,"ifc4_1.txt")))
