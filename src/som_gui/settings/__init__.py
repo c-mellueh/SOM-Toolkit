918761d1470343dd6839633c031b27cf9cf3a805
Christoph Mellueh <=>
2023-03-03 16:19:57 +0100

    remove config.ini

--- src/som_gui/settings/__init__.py
+++ src/som_gui/settings/__init__.py
@@ -4,14 +4,26 @@ CONFIG_PATH =  os.path.join(os.path.dirname(__file__),"config.ini")
 
 def set_file_path(path:str):
     config_path = CONFIG_PATH
+    parent_folder = os.path.dirname(config_path)
+    if not os.path.exists(parent_folder):
+        os.mkdir(parent_folder)
     config = ConfigParser()
     config.read(config_path)
+    if not config.has_section("paths"):
+        config.add_section("paths")
     config.set("paths", "file_path", path)
+
     with open(CONFIG_PATH,"w") as f:
         config.write(f)
 
 def get_file_path():
     config_path = CONFIG_PATH
+    if config_path is None:
+        return ""
     config = ConfigParser()
-    config.read(config_path)
-    return config.get("paths", "file_path")
+    if not os.path.exists(str(CONFIG_PATH)):
+        return ""
+    config.read(CONFIG_PATH)
+    if config.has_section("paths"):
+        return config.get("paths", "file_path")
+    return ""
