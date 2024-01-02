b9d39c857d6494377b7454bb2ca275849153a2b7
Christoph Mellueh <=>
2024-01-02 13:16:37 +0100

    add Icon to Popup

--- som_gui/windows/popups.py
+++ som_gui/windows/popups.py
@@ -228,6 +228,7 @@ def req_boq_pset(main_window, words):
 def attribute_mapping(attribute: classes.Attribute):
     parent = QDialog()
     widget = ui_attribute_mapping.Ui_Dialog()
+    parent.setWindowIcon(get_icon())
     widget.setupUi(parent)
     widget.label_name.setText(f"RevitMapping {attribute.name}")
     widget.line_edit_revit_mapping.setText(attribute.revit_name)
