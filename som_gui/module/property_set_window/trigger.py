79d75d65ab0aa46ebec60eadebc99325e765ba1c
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-02-08 15:43:29 +0100

    add value_button clicked

--- som_gui/module/property_set_window/trigger.py
+++ som_gui/module/property_set_window/trigger.py
@@ -17,7 +17,7 @@ def on_new_project():
 
 
 def connect_window(window: PropertySetWindow):
-    window.widget.button_add_line.clicked.connect(lambda: core.add_value_button_clicked(window, tool.PropertySet))
+    window.widget.button_add_line.clicked.connect(lambda: core.add_value_button_clicked(window, tool.PropertySetWindow))
     window.widget.button_add.clicked.connect(
         lambda: core.add_attribute_button_clicked(window, tool.PropertySet, tool.Attribute))
 
