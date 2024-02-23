201499fd0b2b57ad0e68eb4d1e72f2d2a9624189
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-02-23 13:20:13 +0100

    create Menubar entry

---
+++ som_gui/core/modelcheck.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Type
+
+if TYPE_CHECKING:
+    from som_gui.tool import Modelcheck
+
+
+def open_window(modelcheck: Type[Modelcheck]):
+    pass
--- som_gui/module/modelcheck/trigger.py
+++ som_gui/module/modelcheck/trigger.py
@@ -1,5 +1,7 @@
+from som_gui import tool
+from som_gui.core import modelcheck as core
 def connect():
-    pass
+    tool.MainWindow.add_action("Modelcheck/Interne Modellprüfung", lambda: core.open_window(tool.Modelcheck))
 
 
 def on_new_project():
