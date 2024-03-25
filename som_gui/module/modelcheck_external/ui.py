2ac0ad1db15a33f27c2308fe0add539d0a45ad58
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-03-25 15:13:17 +0100

    remove old reference

--- som_gui/module/modelcheck_external/ui.py
+++ som_gui/module/modelcheck_external/ui.py
@@ -1,16 +1,11 @@
 from __future__ import annotations
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QTreeView, QMainWindow
-from PySide6.QtGui import QStandardItemModel
-from som_gui.module import modelcheck_window
+from PySide6.QtWidgets import QVBoxLayout, QMainWindow
 from som_gui.icons import get_icon
-from .window import Ui_MainWindow
 
 
 class ModelcheckExternalWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        # self.widget = Ui_MainWindow()
-        # self.widget.setupUi(self)
         self.setWindowIcon(get_icon())
         self.vertical_layout = QVBoxLayout(self)
         self.resize(1139, 720)
