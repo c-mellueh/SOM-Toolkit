0a89237ad5b2d73861402a7611781bb864ae811a
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-06-07 12:02:08 +0200

    bump Version

--- som_gui/__init__.py
+++ som_gui/__init__.py
@@ -6,7 +6,7 @@ from .filehandling import export as fh_export
 if TYPE_CHECKING:
     from som_gui.main_window import MainWindow, Ui_MainWindow
 
-__version__ = "2.11.3"
+__version__ = "2.12.0"
 
 import importlib
 
