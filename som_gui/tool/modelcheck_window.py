7c6fb7a23e0996885556a24b0d21a4b5cdc63d55
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-08-05 13:20:15 +0200

    fix modelcheck bug

--- som_gui/tool/modelcheck_window.py
+++ som_gui/tool/modelcheck_window.py
@@ -95,7 +95,8 @@ class ModelcheckWindow(som_gui.core.tool.ModelcheckWindow):
 
     @classmethod
     def create_import_runner(cls, ifc_import_path: str):
-        runner = tool.IfcImporter.create_runner(ifc_import_path)
+        status_label = cls.get_properties().status_label
+        runner = tool.IfcImporter.create_runner(status_label, ifc_import_path)
         cls.get_properties().ifc_import_runners.append(runner)
         return runner
 
@@ -126,8 +127,11 @@ class ModelcheckWindow(som_gui.core.tool.ModelcheckWindow):
 
     @classmethod
     def read_inputs(cls):
+        widget = cls.get_properties().ifc_import_widget
+        ifc_paths = tool.IfcImporter.get_ifc_paths(widget)
         export_path = cls.get_properties().export_line_edit.text()
-        ifc_paths, main_pset, main_attribute = tool.IfcImporter.read_inputs(cls.get_properties().ifc_import_widget)
+        main_pset = tool.IfcImporter.get_main_pset(widget)
+        main_attribute = tool.IfcImporter.get_main_attribute(widget)
         return ifc_paths, export_path, main_pset, main_attribute
 
     @classmethod
