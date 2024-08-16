d005952a011197a6ce66dcabcb2efa114c54bb57
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-08-16 14:46:21 +0200

    remove trigger

--- som_gui/module/predefined_property_set/trigger.py
+++ som_gui/module/predefined_property_set/trigger.py
@@ -59,7 +59,3 @@ def repaint_window():
 
 def accept():
     core.close_window(tool.PredefinedPropertySet)
-
-
-def compare_psetselection_changed(widget):
-    core.compare_pset_selection_changed(widget, tool.PredefinedPropertySetCompare, tool.AttributeCompare)
