a7db9650cd60975bf871431f83c05140ecdf7300
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-09-24 17:36:12 +0200

    finish add module helper

--- som_gui/_add_module.py
+++ som_gui/_add_module.py
@@ -3,8 +3,9 @@ import os
 import logging
 
 
-def main(name: str):
+def create_module(name):
     module_path = os.path.abspath(os.path.join(os.curdir, "module", name))
+
     if os.path.exists(module_path):
         logging.warning("Module already exists")
     else:
@@ -24,11 +25,11 @@ def register():
 
 def load_ui_triggers():
     trigger.connect()
-    
+
 
 def on_new_project():
     trigger.on_new_project()
-    
+
 """)
 
     prop_path = os.path.join(module_path, "prop.py")
@@ -40,7 +41,7 @@ def on_new_project():
 
 class {name.title()}Properties:
     pass
-    
+
 """)
     trigger_path = os.path.join(module_path, "trigger.py")
     if os.path.exists(trigger_path):
@@ -55,7 +56,7 @@ from typing import TYPE_CHECKING
 
 def connect():
     pass
-    
+
 def on_new_project():
     pass
 """)
@@ -65,8 +66,59 @@ def on_new_project():
     with open(ui_path, "w") as f:
         f.write(f"""
 
+    """)
+
+
+def create_core(name: str):
+    core_path = os.path.abspath(os.path.join(os.curdir, "core", f"{name}.py"))
+    if os.path.exists(core_path):
+        logging.warning("core.py already exists")
+    with open(core_path, "w") as f:
+        f.write(
+            f"""from __future__ import annotations
+
+from typing import TYPE_CHECKING, Type
 """)
 
+
+def create_tool(name: str):
+    tool_path = os.path.abspath(os.path.join(os.curdir, "tool", f"{name}.py"))
+    if os.path.exists(tool_path):
+        logging.warning("tool.py already exists")
+    with open(tool_path, "w") as f:
+        f.write(
+            f"""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import logging
+
+import som_gui.core.tool
+import som_gui
+
+if TYPE_CHECKING:
+    from som_gui.module.{name}.prop import {name.title()}Properties
+    
+    
+class {name.title()}(som_gui.core.tool.{name.title()}):
+    @classmethod
+    def get_properties(cls) -> {name.title()}Properties:
+        return som_gui.{name.title()}Properties
+"""
+        )
+
+
+def update_tools():
+    file_path = "core/tool.py"
+    from som_gui import _update_tools
+    with open(file_path, "w") as f:
+        _update_tools.main(f)
+
+
+def main(name: str):
+    create_core(name)
+    create_tool(name)
+    create_module(name)
+    update_tools()
 
 if __name__ == "__main__":
     module_name = "settings"
