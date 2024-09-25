2d08ae31471e10d967d3ec50eae3e214c2e10f8e
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-09-25 20:14:59 +0200

    add Buttonbox to ui

--- som_gui/module/settings/ui.py
+++ som_gui/module/settings/ui.py
@@ -1,6 +1,6 @@
-from PySide6.QtWidgets import QTabWidget, QVBoxLayout, QDialog
-
-
+from PySide6.QtWidgets import QTabWidget, QVBoxLayout, QDialog, QDialogButtonBox
+from PySide6.QtCore import Qt
+from som_gui.icons import get_icon
 class Dialog(QDialog):
     def __init__(self):
         super().__init__()
@@ -8,3 +8,12 @@ class Dialog(QDialog):
         self.tab_widget = QTabWidget(self)
         self.tab_widget.setTabPosition(QTabWidget.TabPosition.West)
         self.layout().addWidget(self.tab_widget)
+        self.button_box = QDialogButtonBox(self)
+        self.button_box.setOrientation(Qt.Orientation.Horizontal)
+        self.button_box.setStandardButtons(QDialogButtonBox.StandardButton.Cancel | QDialogButtonBox.StandardButton.Ok)
+        self.button_box.accepted.connect(Dialog.accept)
+        self.button_box.rejected.connect(Dialog.reject)
+        self.layout().addWidget(self.button_box)
+
+        self.setWindowTitle('Settings')
+        self.setWindowIcon(get_icon())
