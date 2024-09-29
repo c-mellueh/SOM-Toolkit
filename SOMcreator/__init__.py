ddd6882756bd876063787f30321bd7dfdfd749fc
c-mellueh <christoph@mellueh.de>
2024-09-29 15:35:51 +0200

    make SOMcreator uploadable to pypi

--- SOMcreator/__init__.py
+++ SOMcreator/__init__.py
@@ -1,8 +1,8 @@
-from .classes import PropertySet, Object, Project, Attribute, Aggregation
+from .classes import PropertySet, Object, Project, Attribute, Aggregation, UseCase, Phase
 from .external_software import desite, allplan, revit, vestra, card1
 from .constants import json_constants, value_constants
 from .tools import merge_projects
 from .core import export_excel as excel
 
-__version__ = "1.7.8"
+__version__ = "1.7.7"
 active_project = None
