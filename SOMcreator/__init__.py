4243855bfa467f09dc6c206a7a300fceebeb2376
c-mellueh <christoph@mellueh.de>
2024-10-01 08:22:01 +0200

    move Attribute to datastructures folder

--- SOMcreator/__init__.py
+++ SOMcreator/__init__.py
@@ -1,8 +1,8 @@
-from .datastructure import Project, Object, PropertySet
-
-from .classes import Attribute, Aggregation, UseCase, Phase
-from .external_software import desite, allplan, revit, vestra, card1
 from .constants import json_constants, value_constants
+from .datastructure import Project, Object, PropertySet, Attribute
+
+from .classes import Aggregation, UseCase, Phase
+from .external_software import desite, allplan, revit, vestra, card1
 from .tools import merge_projects
 from .core import export_excel as excel
 
--- SOMcreator/datastructure/__init__.py
+++ SOMcreator/datastructure/__init__.py
@@ -1,3 +1,4 @@
 from .project import Project
 from .object_ import Object
 from .propertyset import PropertySet
+from .attribute import Attribute
