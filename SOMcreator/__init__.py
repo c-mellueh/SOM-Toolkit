003c1b6b027471cb0328f81e84e638de1a5007ca
c-mellueh <christoph@mellueh.de>
2025-02-28 10:04:55 +0100

    bump Version

--- SOMcreator/__init__.py
+++ SOMcreator/__init__.py
@@ -13,5 +13,5 @@ from .exporter import desite
 from .util.project import merge_projects
 from .exporter.excel.core import export as export_excel
 
-__version__ = "1.8.2"
+__version__ = "1.9.0"
 active_project = None
