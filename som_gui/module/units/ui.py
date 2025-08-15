85fa92e5713e0d1527625cb5221eb3e370e306a3
c-mellueh <christoph@mellueh.de>
2025-08-15 10:47:50 +0200

    hide deselcted units

--- som_gui/module/units/ui.py
+++ som_gui/module/units/ui.py
@@ -1,9 +1,10 @@
-from PySide6.QtCore import QRect, QSize, Qt,QAbstractItemModel,QModelIndex
+from PySide6.QtCore import QRect, QSize, Qt, QAbstractItemModel, QModelIndex
 from PySide6.QtWidgets import QHeaderView, QWidget, QComboBox, QTreeView
 from PySide6.QtGui import QStandardItemModel, QStandardItem
 from som_gui.module.units import trigger
 from .qt import ui_UnitSettings
 from som_gui import tool
+
 
 class UnitSettings(QWidget):
     def __init__(self, *args, **kwargs):
@@ -11,6 +12,7 @@ class UnitSettings(QWidget):
         self.ui = ui_UnitSettings.Ui_UnitSettings()
         self.ui.setupUi(self)
         trigger.unit_settings_created(self)
+
 
 # class UnitComboBox(QComboBox):
 #     def __init__(self, *args, **kwargs):
@@ -28,7 +30,7 @@ class UnitComboBox(QComboBox):
         self.tree_view = QTreeView()
         self.setView(self.tree_view)
         d = tool.Units.get_units_dict()
-        self.mod =ComboboxItemModel(d)
+        self.mod = ComboboxItemModel(d)
         self.setModel(self.mod)
         self.tree_view.header().setVisible(False)
         self.tree_view.expanded.connect(lambda: setattr(self, "is_locked", True))
@@ -40,7 +42,7 @@ class UnitComboBox(QComboBox):
             self.index_changed
         )  # Populate the model with hierarchical data
         self.setEditable(False)
-        #self.add_items()
+        # self.add_items()
         self.is_locked = False
         self.tree_view.setMinimumHeight(40)
 
@@ -49,7 +51,6 @@ class UnitComboBox(QComboBox):
 
         trigger.repaint_unit_combobox(self)
         return super().paintEvent(e)
-
 
     def hidePopup(self):
         # Allow the popup to close if the combo box loses focus
@@ -72,14 +73,16 @@ class UnitComboBox(QComboBox):
             unit = item
 
         self.setCurrentText(unit["Code"])
-        trigger.update_current_unit(self.parent(),unit)
+        trigger.update_current_unit(self.parent(), unit)
+
 
 class SettingsItemModel(QAbstractItemModel):
-    def __init__(self,data_dict, *args, **kwargs):
+    def __init__(self, data_dict, *args, **kwargs):
         super().__init__(*args, **kwargs)
         from som_gui.resources.data import UNIT_PATH
         import json
-        self.data_dict =data_dict
+
+        self.data_dict = data_dict
         # self.setHorizontalHeaderLabels(["Unit", "Prefix"])
         # self.setColumnCount(2)
 
@@ -89,19 +92,20 @@ class SettingsItemModel(QAbstractItemModel):
         else:
             data = parent.internalPointer()
             if data["type"] == "quantity":
-                return len(parent.internalPointer()["children"]+ parent.internalPointer()["units"])
+                return len(
+                    parent.internalPointer()["children"]
+                    + parent.internalPointer()["units"]
+                )
             return 0
-
 
     def columnCount(self, parent=QModelIndex()):
         return 3
 
-    def data(self, index:QModelIndex, role=Qt.ItemDataRole.DisplayRole):
+    def data(self, index: QModelIndex, role=Qt.ItemDataRole.DisplayRole):
         if not index.isValid():
             return None
-        
 
-        if role not in (Qt.ItemDataRole.DisplayRole,Qt.ItemDataRole.CheckStateRole):
+        if role not in (Qt.ItemDataRole.DisplayRole, Qt.ItemDataRole.CheckStateRole):
             return None
         data = index.internalPointer()
         if Qt.ItemDataRole.DisplayRole == role:
@@ -120,8 +124,8 @@ class SettingsItemModel(QAbstractItemModel):
                 return None
             if data["type"] == "quantity":
                 return None
-            return tool.Util.bool_to_checkstate(data.get("is_checked", True))
-        
+            return tool.Util.bool_to_checkstate(data.get("is_active", True))
+
     def setData(self, index: QModelIndex, value, role: Qt.ItemDataRole):
         if not index.isValid():
             return False
@@ -129,13 +133,13 @@ class SettingsItemModel(QAbstractItemModel):
         if role != Qt.ItemDataRole.CheckStateRole:
             return False
         data = index.internalPointer()
-        data["is_checked"] = bool(value)
+        data["is_active"] = bool(value)
         return True
-    
+
     def index(self, row: int, column: int, parent=QModelIndex()):
         # logging.debug(f"request Index {row}:{column} {parent}")
         if not parent.isValid():
-            if 0>row >= len(self.data_dict):
+            if 0 > row >= len(self.data_dict):
                 return QModelIndex()
             element = self.data_dict[row]
             element["type"] = "quantity"
@@ -154,7 +158,7 @@ class SettingsItemModel(QAbstractItemModel):
                 child_data["type"] = "quantity"
                 index = self.createIndex(row, column, child_data)
                 return index
-            elif child_count<=row<= child_count + unit_count:
+            elif child_count <= row <= child_count + unit_count:
                 unit_data = units[row - child_count]
                 unit_data["parent"] = parent
                 unit_data["type"] = "unit"
@@ -168,7 +172,7 @@ class SettingsItemModel(QAbstractItemModel):
     def parent(self, index: QModelIndex):
         if not index.isValid():
             return QModelIndex()
-        data =  index.internalPointer()
+        data = index.internalPointer()
         if data is None or data.get("parent") is None:
             return QModelIndex()
         return data["parent"]
@@ -184,41 +188,47 @@ class SettingsItemModel(QAbstractItemModel):
         if column >= self.columnCount():
             return flags
 
-        if index.column() == 2: #and index.internalPointer().get("type") == "unit":
+        if index.column() == 2:  # and index.internalPointer().get("type") == "unit":
             flags |= Qt.ItemFlag.ItemIsUserCheckable
         else:
             flags &= ~Qt.ItemFlag.ItemIsUserCheckable
         return flags
-    
+
+
+def _calculate_row_count(quant):
+    quant_count = len([q for q in quant["children"] if _calculate_row_count(q) > 0])
+    unit_count = len([u for u in quant["units"] if u.get("is_active")])
+    return quant_count + unit_count
+
 
 class ComboboxItemModel(QAbstractItemModel):
-    def __init__(self,data_dict, *args, **kwargs):
+    def __init__(self, data_dict, *args, **kwargs):
         super().__init__(*args, **kwargs)
         from som_gui.resources.data import UNIT_PATH
         import json
-        self.data_dict =data_dict
+
+        self.data_dict = data_dict
         # self.setHorizontalHeaderLabels(["Unit", "Prefix"])
         # self.setColumnCount(2)
 
     def rowCount(self, parent=QModelIndex()):
+
         if not parent.isValid():
-            return len(self.data_dict)
+            return len([q for q in self.data_dict if _calculate_row_count(q) > 0])
         else:
             data = parent.internalPointer()
             if data["type"] == "quantity":
-                return len(parent.internalPointer()["children"]+ parent.internalPointer()["units"])
+                return _calculate_row_count(data)
             return 0
-
 
     def columnCount(self, parent=QModelIndex()):
         return 1
 
-    def data(self, index:QModelIndex, role=Qt.ItemDataRole.DisplayRole):
+    def data(self, index: QModelIndex, role=Qt.ItemDataRole.DisplayRole):
         if not index.isValid():
             return None
-        
 
-        if role not in (Qt.ItemDataRole.DisplayRole,Qt.ItemDataRole.CheckStateRole):
+        if role not in (Qt.ItemDataRole.DisplayRole, Qt.ItemDataRole.CheckStateRole):
             return None
         data = index.internalPointer()
         if Qt.ItemDataRole.DisplayRole != role:
@@ -230,25 +240,28 @@ class ComboboxItemModel(QAbstractItemModel):
             if index.column() == 0:
                 return index.internalPointer().get("Code", "")
         return None
-    
+
     def setData(self, index: QModelIndex, value, role: Qt.ItemDataRole):
         return False
-    
+
     def index(self, row: int, column: int, parent=QModelIndex()):
         # logging.debug(f"request Index {row}:{column} {parent}")
         if not parent.isValid():
-            if 0>row >= len(self.data_dict)-1:
+            first_layer_elements = [q for q in self.data_dict if _calculate_row_count(q)>0]
+            if 0 > row >= len(first_layer_elements):
                 return QModelIndex()
-            element = self.data_dict[row]
+            element = first_layer_elements[row]
             element["type"] = "quantity"
             index = self.createIndex(row, column, element)
             return index
         else:
             parent = parent.siblingAtColumn(0)
             parent_data = parent.internalPointer()
-            children = parent_data["children"]
+            children = [
+                c for c in parent_data["children"] if _calculate_row_count(c) > 0
+            ]
             child_count = len(children)
-            units = parent_data["units"]
+            units = [u for u in parent_data["units"] if u.get("is_active")]
             unit_count = len(units)
             if 0 <= row < child_count:
                 child_data = children[row]
@@ -256,7 +269,7 @@ class ComboboxItemModel(QAbstractItemModel):
                 child_data["type"] = "quantity"
                 index = self.createIndex(row, column, child_data)
                 return index
-            elif child_count<=row<= child_count + unit_count:
+            elif child_count <= row <= child_count + unit_count:
                 unit_data = units[row - child_count]
                 unit_data["parent"] = parent
                 unit_data["type"] = "unit"
@@ -270,7 +283,7 @@ class ComboboxItemModel(QAbstractItemModel):
     def parent(self, index: QModelIndex):
         if not index.isValid():
             return QModelIndex()
-        data =  index.internalPointer()
+        data = index.internalPointer()
         if data is None or data.get("parent") is None:
             return QModelIndex()
         return data["parent"]
